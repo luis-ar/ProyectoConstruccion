6bcc8c1758898f150812f9317ece449332dc0fe0
Luis Arturo <luisdemaryori@gmail.com>
2023-08-20 15:29:52 -0500

    009celular

--- components/ui/BarraRedes.js
+++ components/ui/BarraRedes.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Redes.module.css";
 const BarraRedes = () => {
+  const [hidden, setHidden] = useState(false);
+  const ocultarDiv = () => {
+    setHidden(true);
+    setTimeout(() => {
+      setHidden(false);
+    }, 3000);
+  };
+
   return (
     <>
       <div className={styles.contenedor}>
         <ul>
-          <li>
+          <li className={` ${hidden ? "ocultar" : ""}`} onClick={ocultarDiv}>
             <a href="#">
               <i className="bx bxl-gmail gmail"></i>Gmail
             </a>
           </li>
-          <li>
+          <li className={` ${hidden ? "ocultar" : ""}`} onClick={ocultarDiv}>
             <a href="#">
               <i className="bx bxl-facebook facebook"></i>Facebook
             </a>
           </li>
-          <li>
+          <li className={` ${hidden ? "ocultar" : ""}`} onClick={ocultarDiv}>
             <a href="#">
               <i className="bx bxl-twitter twitter"></i>Twitter
             </a>
           </li>
-          <li>
+          <li className={` ${hidden ? "ocultar" : ""}`} onClick={ocultarDiv}>
             <a href="#">
               <i className="bx bxl-tiktok tiktok"></i>Tiktok
             </a>
