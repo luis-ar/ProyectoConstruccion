2cf00c5ccd6a4588bb2f47c130c445738f4d3af7
Luis Arturo <luisdemaryori@gmail.com>
2023-09-06 00:07:58 -0500

    023celular

--- components/ui/BarraRedes.js
+++ components/ui/BarraRedes.js
@@ -3,25 +3,34 @@ import styles from "@/styles/Redes.module.css";
 const BarraRedes = () => {
   return (
     <>
-      <div className={styles.contenedor} >
+      <div className={styles.contenedor}>
         <ul>
           <li>
-            <a href="#">
-              <i className="bx bxl-gmail gmail"></i>Gmail
+            <a
+              href="https://instagram.com/inmobiliaria_future?utm_source=qr&igshid=MThlNWY1MzQwNA=="
+              target="_blank"
+            >
+              <i className="bx bxl-instagram instagram"></i>Instagram
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              href="https://www.facebook.com/profile.php?id=61550705684336"
+              target="_blank"
+            >
               <i className="bx bxl-facebook facebook"></i>Facebook
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://twitter.com/FutureRenacer" target="_blank">
               <i className="bx bxl-twitter twitter"></i>Twitter
             </a>
           </li>
           <li>
-            <a href="#">
+            <a
+              href="https://www.tiktok.com/@futureelverdaderorenacer"
+              target="_blank"
+            >
               <i className="bx bxl-tiktok tiktok"></i>Tiktok
             </a>
           </li>
